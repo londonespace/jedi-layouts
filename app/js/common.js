4c15a1712c86404d3c26e55717b4a240b8b70406
Peter Ilyash <p.ilyash.j@gmail.com>
2020-08-11 23:57:37 +0300

    Add animated header-menu using plugin 'Mmenu'

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,25 @@
-$(function() {
+$(function () {
 
-	// Custom JS
+	$('#my-menu').mmenu({
+		extensions: ['theme-black', 'effect-menu-slide', 'pagedim-black'],
+
+		navbar: {
+			title: '<img src="img/dest/logo-1.svg" alt="Салон красоты Митлер">'
+		},
+
+		offCanvas: {
+			position: 'right'
+		}
+	});
+
+	let menuAPI = $('#my-menu').data('mmenu');
+
+	menuAPI.bind('opened', function () {
+		$('.hamburger').addClass('is-active');
+	});
+
+	menuAPI.bind('closed', function () {
+		$('.hamburger').removeClass('is-active');
+	});
 
 });
