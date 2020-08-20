924716b69d842bb213501649d8b81185d7690172
Peter Ilyash <p.ilyash.j@gmail.com>
2020-08-20 16:40:35 +0300

    Add the services section and carousel jQuery plugin 'owl-carousel' is used to create the carousel

--- app/js/common.js
+++ app/js/common.js
@@ -22,12 +22,14 @@ $(function () {
 		$('.hamburger').removeClass('is-active');
 	});
 
+	$('.carousel-services').on('initialized.owl.carousel', () => setTimeout(carouselService));
+
 	$('.carousel-services').owlCarousel({
-		loop: true,
 		nav: true,
 		smartSpeed: 700,
 		navText: ['<i class="fa fa-angle-double-left"></i>',
 			'<i class="fa fa-angle-double-right"></i>'],
+		dots: false,
 		responsiveClass: true,
 		responsive: {
 
@@ -45,4 +47,26 @@ $(function () {
 
 		}
 	});
+
+	function carouselService() {
+		let items = $('#carousel-services').find('[class$="item"]');
+
+		items.each(function () {
+			let itemHeight = $(this).find('.carousel-services-content').outerHeight();
+			let itemImage = $(this).find('[class$="image"]');
+			console.log(itemImage);
+
+			itemImage.css('min-height', itemHeight);
+		});
+	}
+
+	$('.carousel-services-headling').each(function () {
+		$(this).html($(this).html().replace(/(\S+)\s*$/, '<span>$1</span>'));
+	});
+
+	function onResize() {
+		$('.carousel-services-content').equalHeights();
+	}
+
+	window.on('resize', onResize);
 });
