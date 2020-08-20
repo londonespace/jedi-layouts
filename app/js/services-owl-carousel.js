2f217fd2b3ee3e962528abc741bb223f960504fe
Peter Ilyash <p.ilyash.j@gmail.com>
2020-08-20 17:19:50 +0300

    Refactor common.js code
    It is split into 2 files. The functions and settings of 'mmenu' is in
    mmenu.js and the owl-carousel's in services-owl-carousel.js.
    
    Also some functions is renamed and optimized.

--- app/js/services-owl-carousel.js
+++ app/js/services-owl-carousel.js
@@ -1,28 +1,5 @@
 $(function () {
-
-	$('#my-menu').mmenu({
-		extensions: ['theme-black', 'effect-menu-slide', 'pagedim-black'],
-
-		navbar: {
-			title: '<img src="img/dest/logo-1.svg" alt="Салон красоты Митлер">'
-		},
-
-		offCanvas: {
-			position: 'right'
-		}
-	});
-
-	let menuAPI = $('#my-menu').data('mmenu');
-
-	menuAPI.bind('opened', function () {
-		$('.hamburger').addClass('is-active');
-	});
-
-	menuAPI.bind('closed', function () {
-		$('.hamburger').removeClass('is-active');
-	});
-
-	$('.carousel-services').on('initialized.owl.carousel', () => setTimeout(carouselService));
+	$('.carousel-services').on('initialized.owl.carousel', () => setTimeout(setHeightForCarouselImages));
 
 	$('.carousel-services').owlCarousel({
 		nav: true,
@@ -48,25 +25,27 @@ $(function () {
 		}
 	});
 
-	function carouselService() {
+	$('.carousel-services-headling').each(wrapLastWordInSpan);
+
+	window.on('resize', alignCarouselItemsHeight);
+
+	function setHeightForCarouselImages() {
 		let items = $('#carousel-services').find('[class$="item"]');
+		console.log(1);
 
 		items.each(function () {
 			let itemHeight = $(this).find('.carousel-services-content').outerHeight();
 			let itemImage = $(this).find('[class$="image"]');
-			console.log(itemImage);
 
 			itemImage.css('min-height', itemHeight);
 		});
 	}
 
-	$('.carousel-services-headling').each(function () {
+	function wrapLastWordInSpan() {
 		$(this).html($(this).html().replace(/(\S+)\s*$/, '<span>$1</span>'));
-	});
-
-	function onResize() {
-		$('.carousel-services-content').equalHeights();
 	}
 
-	window.on('resize', onResize);
+	function alignCarouselItemsHeight() {
+		$('.carousel-services-content').equalHeights();
+	}
 });
