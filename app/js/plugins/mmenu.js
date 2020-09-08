c114a7bfa04348251d60969903d87e463829b8dc
Peter Ilyash <p.ilyash.j@gmail.com>
2020-09-08 11:36:40 +0300

    Fix several layout problems
    Also then if empty link is clicked, there isn't going up the page.

--- app/js/plugins/mmenu.js
+++ app/js/plugins/mmenu.js
@@ -3,7 +3,7 @@ $(function () {
     extensions: ['theme-black', 'effect-menu-slide', 'pagedim-black'],
 
     navbar: {
-      title: '<img src="img/dest/logo-1.svg" alt="Салон красоты Митлер">'
+      title: '<img src="img/logo-1.svg" alt="Салон красоты Митлер">'
     },
 
     offCanvas: {
